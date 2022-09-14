6c4a06defeb28fa4d3382d12542aaaee02e0a05a
Julie Heckler <july.heckler@gmail.com>
2022-09-14 23:23:48 +0200

    project initialization

---
+++ src/components/ProjectCard/index.js
@@ -0,0 +1,9 @@
+
+export function ProjectCard(){
+
+    return(
+        <div className='projectCard_Wrapper'>
+
+        </div>
+    )
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import { ProjectCard } from '../../components/ProjectCard'
+
+export function Home(){
+
+    return(
+        <div className='home_wrapper'>
+            <div className='bandGallery_Wrapper'>
+                <ProjectCard />
+            </div>
+        </div>
+    )
+}
